36d087c5a9ae725d9b1ddac78c7a253c3ecd0260
WalnutsAreCool <claadam@nuevaschool.org>
2016-10-21 08:43:12 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 
 onNewListItem("product-list", function(product) {
   var nameHeader = $("<h1 class='product-name'>" + product.name + "</h1>")
-  var image = $("<img class='product-image'>").attr("src", "http://geekplanet.ca/portal/wp-content/uploads/2012/04/strawberrymonster.jpg")  
+  var image = $("<img class='product-image' height='200'>").attr("src", product.picture)  
+  console.log(product.picture)
 
   $(".product-list-container").append(nameHeader, image)
 })
